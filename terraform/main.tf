55cad075443e4575ac2a52f860e971dcebd405a4
Mohammad Adha Bin Sahar <adhasahar@Mohammads-MacBook-Pro.local>
2024-11-15 19:00:07 +0800

    Init

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "example-nginx"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
