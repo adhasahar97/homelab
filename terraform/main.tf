dd8ec992d48ac8ed5a674e021d5cfad138f501f2
Mohammad Adha Bin Sahar <adhasahar@Mohammads-MacBook-Pro.local>
2024-11-15 19:06:26 +0800

    Add required provider configuration for Docker in Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,17 @@
 
-provider "docker" {}
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.12.2"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
 
 resource "docker_image" "nginx" {
   name = "nginx:latest"
