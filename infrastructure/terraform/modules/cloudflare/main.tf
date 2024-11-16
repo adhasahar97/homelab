a5f4c07b9030982f6e8bbdf445a892121c6d2c0e
Mohammad Adha Bin Sahar <adhasahar@Mohammads-MacBook-Pro.local>
2024-11-16 18:52:38 +0800

    cloudflare_tunnel is deprecated. Use cloudflare_zero_trust_tunnel_cloudflared instead

--- infrastructure/terraform/modules/cloudflare/main.tf
+++ infrastructure/terraform/modules/cloudflare/main.tf
@@ -19,7 +19,7 @@ resource "random_password" "tunnel_secret" {
   length = 64
 }
 
-resource "cloudflare_tunnel" "k8s-tunnel" {
+resource "cloudflare_zero_trust_tunnel_cloudflared" "k8s-tunnel" {
   account_id = "5b2f562a73bc2d4816ec68f4b653e38d"
   name       = "homelab-k8s-tunnel"
   secret     = base64sha256(random_password.tunnel_secret.result)
