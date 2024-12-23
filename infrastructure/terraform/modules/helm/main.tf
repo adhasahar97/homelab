1f08fa94fde414d3a97e68372a693e6c87502376
Mohammad Adha Bin Sahar <adhasahar@Mohammads-MacBook-Pro.local>
2024-12-23 17:36:19 +0800

    Wrong chart name

--- infrastructure/terraform/modules/helm/main.tf
+++ infrastructure/terraform/modules/helm/main.tf
@@ -7,7 +7,7 @@ provider "helm" {
 resource "helm_release" "nginx" {
     name       = "nginx"
     repository = "https://kubernetes.github.io/ingress-nginx/"
-    chart      = "nginx"
+    chart      = "ingress-nginx"
     version    = "4.11.3"
     namespace  = "ingress"
     create_namespace = true
