1841b25421225a6b7a59930b8ce0d6633fd9d883
Mohammad Adha Bin Sahar <adhasahar@Mohammads-MacBook-Pro.local>
2025-01-13 02:35:25 +0800

    Debug

--- infrastructure/terraform/modules/helm/releases.tf
+++ infrastructure/terraform/modules/helm/releases.tf
@@ -6,9 +6,10 @@ locals {
       chart      = "argo-cd"
       namespace  = "argocd"
       version    = "7.7.15"
-      yaml_values = [templatefile("${path.module}/values/values-argocd.yaml", {
-
-      })]
+      values_path = "${path.module}/values/values-argocd.yaml"
+      yaml_values = {
+        
+      }
     },
     "cloudflare-tunnel" = {
       name       = "cloudflare-tunnel-ingress-controller"
@@ -16,11 +17,12 @@ locals {
       chart      = "cloudflare-tunnel-ingress-controller"
       namespace  = "cloudflare-tunnel"
       version    = "0.0.16"
-      yaml_values = [templatefile("${path.module}/values/values-cloudflare-tunnel.yaml", {
+      values_path = "${path.module}/values/values-cloudflare-tunnel.yaml"
+      yaml_values = {
         "cloudflare.apiToken"   = var.cloudflare_api_token
         "cloudflare.accountId"  = var.cloudflare_tunnel_account_id
         "cloudflare.tunnelName" = var.cloudflare_tunnel_name
-      })]
+      }
     }
   }
 }
@@ -35,5 +37,5 @@ resource "helm_release" "release" {
   namespace        = each.value.namespace
   create_namespace = true
 
-  values = each.value.yaml_values
+  values = [templatefile(each.value.values_path, each.value.yaml_values)]
 }
