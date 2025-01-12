81325674207327968f63e5b8d31c7e786b49a83b
Mohammad Adha Bin Sahar <adhasahar@Mohammads-MacBook-Pro.local>
2025-01-13 02:17:22 +0800

    Debug

--- infrastructure/terraform/modules/helm/variables.tf
+++ infrastructure/terraform/modules/helm/variables.tf
@@ -1,7 +1,7 @@
 variable "cloudflare_api_token" {
   description = "The token used to authenticate with Cloudflare's Zero Trust Tunneling service"
   type        = string
-  sensitive = true
+  # sensitive = true
 }
 variable "cloudflare_tunnel_account_id" {
   description = "The account ID associated with the Cloudflare Zero Trust Tunneling service"
